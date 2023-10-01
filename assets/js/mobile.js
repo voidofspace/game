71289b4c7156ee9ecc03b0cfe97876385cdb5f2e
voidofspace <146674705+voidofspace@users.noreply.github.com>
2023-10-01 23:49:18 +0300

    Add files via upload

---
+++ assets/js/mobile.js
@@ -0,0 +1,1 @@
+/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)&&(setTimeout(function(){alert("This site is not optimized for mobile devices. Please use a desktop computer to view this site.")},2e3),document.getElementById("navbar").style.display="none",document.getElementById("burger").style.display="none");
